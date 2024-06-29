2d1c578382e9c2f6ad3f02a13c980ff4d69861fd
Sondos Taha <129762487+SondosAhmedTaha@users.noreply.github.com>
2024-06-29 23:08:19 +0300

    Update ex1.asm

--- ex1.asm
+++ ex1.asm
@@ -1,36 +1,45 @@
-.global _start
-
 .section .text
+.global _start
 _start:
-	test length, length
-  	jz Not_Legal_HW1 
-	js Not_Legal_HW1
-	test Index, Index
-	js Not_Legal_HW1
+    test length, length
+    jz Not_Legal_HW1 
+    js Not_Legal_HW1
+    test Index, Index
+    js Not_Legal_HW1
 
-	movq length, %rbx   
-	shrq $32, %rbx        
-	test %ebx, %ebx      
-	jne Not_Legal_HW1   
+    movq length, %rbx   
+    shrq $32, %rbx        
+    test %ebx, %ebx      
+    jne Not_Legal_HW1   
   
-	movq Index, %rbx   
-	shrq $32, %rbx        
-	test %ebx, %ebx      
-	jne Not_Legal_HW1   
-	
-	cmpq Index, length
-	jae Not_Legal_HW1
+    movq Index, %rbx   
+    shrq $32, %rbx        
+    test %ebx, %ebx      
+    jne Not_Legal_HW1
+   
+    # Check if Index < length
+    movq length, %rbx
+    cmpq %rbx, Index
+    jae Not_Legal_HW1
 
-	movq Adress, %rbx
-	movl Index, %ecx
-	movl (%rbx,%ecx,4), %eax  
-	movl %eax, num
-	mov Legal,#1
-	jmp End_HW1
+    # Check if Address is non-null
+    movq Address, %rbx
+    test %rbx, %rbx
+    jz Not_Legal_HW1
+
+    # Check alignment (optional, if required)
+    # test $0x3, %rbx    # Check for 4-byte alignment, for example
+    # jnz Not_Legal_HW1
+
+    # Access array and set num
+    movl Index, %ecx
+    movl (%rbx,%rcx,4), %eax  
+    movl %eax, num
+    movb $1, Legal
+    jmp End_HW1
 
 Not_Legal_HW1:
-		mov Legal,#0
+    movb $0, Legal
 
-End_HW1:	
-
-
+End_HW1:
+    nop
