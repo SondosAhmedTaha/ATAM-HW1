b50d79c038cc7a38e8476d7ee614004f2d85d46b
Sondos Taha <129762487+SondosAhmedTaha@users.noreply.github.com>
2024-07-05 22:58:01 +0300

    Update ex3.asm

--- ex3.asm
+++ ex3.asm
@@ -3,22 +3,40 @@
 
 _start:
     movq root(%rip), %rdi          # מצביע לשורש של העץ
-    xor %rbx, %rbx           # איפוס מונה הצמתים
+    xor %rax, %rax           # איפוס מונה הצמתים
     xor %rcx, %rcx           # איפוס מונה העלים
     xor %rdx, %rdx           # איפוס מונה העומק
 
 traverse_tree_HW1:
-    inc %rbx
-    movq 0(%rdi), %r9   ; Load first child pointer
-    test %r9, %r9       ; Check if it's NULL
-    jz count_leaf       ; If NULL, it's a leaf
-    movq %r9, %rdi      ; Move to the first child node
-    jmp traverse_tree_HW1   ; Continue traversal
+    inc %rax
+    movq 0(%rdi), %r9   
+    test %r9, %r9      
+    jz count_leaf_HW1       
+    movq %r9, %rdi      
+    jmp traverse_tree_HW1   
 
 count_leaf_HW1:
     inc %rcx
     movq (%rdi), %rdi   
     cmp root, %rdi
     jne traverse_tree_HW1 
+
+End_Scan_HW1:
+    xor %rdx, %rdx          
+    div %rcx              
+    cmp $3, %rax
+    jg Not_Rich_HW1
+    je Equal_HW1        
+    jmp Rich_HW1
+Not_Rich_HW1:
+    movb $0,rich
+    jmp End_HW1
+Rich_HW1:
+    movb $1, rich
+    jmp End_HW1
+Equal_HW1: 
+    cmp $0, %rdx
+    je Rich_HW1
+    jmp Not_Rich_HW1
     
 End_HW1:
