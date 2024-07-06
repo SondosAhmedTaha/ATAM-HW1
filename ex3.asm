0eca2e769c5d4220550cd9427046d31e5e4ebc39
Sondos Taha <129762487+SondosAhmedTaha@users.noreply.github.com>
2024-07-07 02:47:19 +0300

    Update ex3.asm

--- ex3.asm
+++ ex3.asm
@@ -3,128 +3,125 @@
 .section .text
 _start:
 #your code here
-    xor %rax, %rax       # Clear rax (node counter)
-    xor %rcx, %rcx       # Clear rcx (leaf counter)
+    xor %rax, %rax  //vertec counter    
+    xor %rcx, %rcx  //leaves counter 
 
-
-    # Start traversal from root
-    mov $root, %r10
-    cmpq $0, (%r10)
-    je Not_Rich
-level1:
+    movq root(%rip), %rdi
+    cmpq $0, (%rdi)
+    je Not_Rich_HW1
+hight1_HW1:
     addq $1, %rax
-    cmpq $0, (%r10)
-    jne level1_loop
+    cmpq $0, (%rdi)
+    jne hight1_loop
     addq $1, %rcx
-    jmp traverse_complete
+    jmp Scan_Done_HW1
 
-level1_loop:
-    movq (%r10), %r11
+hight1_loop:
+    movq (%rdi), %r8
+    cmpq $0, %r8
+    je Is_leaf1
+    addq $1, %rax
+hight1_next:
+    addq $8, %rdi
+    jmp hight1_loop
+
+Is_leaf1:
+    cmpq (root), %rdi
+    jne Scan_Done_HW1
+    addq $1, %rcx
+    jmp Scan_Done_HW1
+
+hight2_loop:
+    movq (%r8), %r9
+    cmpq $0, %r9
+    je Is_leaf2
+    addq $1, %rax
+hight2_next:
+    addq $8, %r8
+    jmp hight2_loop
+
+Is_leaf2:
+    cmpq (%rdi), %r8
+    jne hight1_next
+    addq $1, %rcx
+    jmp hight1_next
+
+
+hight3_loop:
+    movq (%r9), %r11
     cmpq $0, %r11
-    je check_leaf1
+    je Is_leaf3
     addq $1, %rax
 
-level2_loop:
-    movq (%r11), %r12
-    cmpq $0, %r12
-    je check_leaf2
-    addq $1, %rax
+hight3_next:
+    addq $8, %r9
+    jmp hight3_loop
 
-level3_loop:
-    movq (%r12), %r13
-    cmpq $0, %r13
-    je check_leaf3
-    addq $1, %rax
-
-level4_loop:
-    movq (%r13), %r14
-    cmpq $0, %r14
-    je check_leaf4
-    addq $1, %rax
-
-level5_loop:
-    movq (%r14), %r15
-    cmpq $0, %r15
-    je check_leaf5
-    addq $1, %rax
-
-level6:
-    addq $1, %rax
+Is_leaf3:
+    cmpq (%r8), %r9
+    jne hight2_next
     addq $1, %rcx
-    addq $8, %r15
+    jmp hight2_next 
 
-level5_next:
-    addq $8, %r14
-    jmp level5_loop
+hight4_loop:
+    movq (%r11), %r10
+    cmpq $0, %r10
+    je Is_leaf4
+    addq $1, %rax
 
-level4_next:
-    addq $8, %r13
-    jmp level4_loop
-
-level3_next:
-    addq $8, %r12
-    jmp level3_loop
-
-level2_next:
+hight4_next:
     addq $8, %r11
-    jmp level2_loop
+    jmp hight4_loop
 
-level1_next:
+Is_leaf4:
+    cmpq (%r9), %r11
+    jne hight2_next
+    addq $1, %rcx
+    jmp hight3_next
+
+hight5_loop:
+    movq (%r10), %r12
+    cmpq $0, %r12
+    je Is_leaf5
+    addq $1, %rax
+
+hight5_next:
     addq $8, %r10
-    jmp level1_loop
+    jmp hight5_loop
 
-check_leaf1:
-    cmpq (root), %r10
-    jne traverse_complete
+Is_leaf5:
+    cmpq (%r11), %r10
+    jne hight2_next
     addq $1, %rcx
-    jmp traverse_complete
+    jmp hight4_next
 
-check_leaf2:
-    cmpq (%r10), %r11
-    jne level1_next
+hight6:
+    addq $1, %rax
     addq $1, %rcx
-    jmp level1_next
+    addq $8, %r12
 
-check_leaf3:
-    cmpq (%r11), %r12
-    jne level2_next
-    addq $1, %rcx
-    jmp level2_next 
-
-check_leaf4:
-    cmpq (%r12), %r13
-    jne level2_next
-    addq $1, %rcx
-    jmp level3_next
-
-check_leaf5:
-    cmpq (%r13), %r14
-    jne level2_next
-    addq $1, %rcx
-    jmp level4_next
-
-traverse_complete:
+Scan_Done_HW1:
     test %rcx, %rcx      # Check if there are any leaves
-    jz Not_Rich          # If no leaves, tree is not rich
+    jz Not_Rich_HW1          # If no leaves, tree is not rich
 
     xor %rdx, %rdx       # Clear rdx for division
     div %rcx             # Divide total nodes by total leaves
     cmp $3, %rax         # Compare the quotient with 3
-    jg Not_Rich          # If quotient > 3, not rich
-    je Equal_Rich        # If quotient == 3, check remainder
-    jmp Rich             # Else, rich
+    jg Not_Rich_HW1          # If quotient > 3, not rich
+    je Equal_Rich_HW1        # If quotient == 3, check remainder
+    jmp Rich_HW1             # Else, rich
 
-Not_Rich:
+Not_Rich_HW1:
     movb $0, rich  # Set rich to 0
-    jmp End
+    jmp End_HW1
 
-Rich:
+Rich_HW1:
     movb $1, rich  # Set rich to 1
-    jmp End
+    jmp End_HW1
 
-Equal_Rich:
+Equal_Rich_HW1:
     cmp $0, %rdx         # Check if remainder is 0
-    je Rich              # If 0, rich
-    jmp Not_Rich         # Else, not rich
+    je Rich_HW1              # If 0, rich
+    jmp Not_Rich_HW1         # Else, not rich
 
-End:
+End_HW1_:
