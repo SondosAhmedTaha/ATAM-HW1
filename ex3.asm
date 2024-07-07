224313320635bd19d69cd038e88fda44b62b0329
Sondos Taha <129762487+SondosAhmedTaha@users.noreply.github.com>
2024-07-07 03:23:46 +0300

    Update ex3.asm

--- ex3.asm
+++ ex3.asm
@@ -1,116 +1,114 @@
-.global _start
-
 .section .text
 _start:
-#your code here
-    xor %rax, %rax  #vertec counter    
-    xor %rcx, %rcx  #leaves counter 
+    xor %rax, %rax       # Clear rax (node counter)
+    xor %rcx, %rcx       # Clear rcx (leaf counter)
 
     movq root(%rip), %rdi
     cmpq $0, (%rdi)
     je Not_Rich_HW1
-hight1_HW1:
+
+height1_HW1:
     addq $1, %rax
     cmpq $0, (%rdi)
-    jne loop1_HW1
+    jne height1_loop_HW1
     addq $1, %rcx
-    jmp Scan_Done_HW1
+    jmp traverse_complete_HW1
 
-loop1_HW1:
+height1_loop_HW1:
     movq (%rdi), %r8
     cmpq $0, %r8
     je Is_leaf1_HW1
     addq $1, %rax
 
-loop2_HW1:
+height2_loop_HW1:
     movq (%r8), %r9
     cmpq $0, %r9
     je Is_leaf2_HW1
     addq $1, %rax
 
-loop3_HW1:
-    movq (%r9), %r11
-    cmpq $0, %r11
+height3_loop_HW1:
+    movq (%r9), %r10
+    cmpq $0, %r10
     je Is_leaf3_HW1
     addq $1, %rax
-loop4_HW1:
-    movq (%r11), %r10
-    cmpq $0, %r10
+
+height4_loop_HW1:
+    movq (%r10), %r11
+    cmpq $0, %r11
     je Is_leaf4_HW1
     addq $1, %rax
-loop5_HW1:
-    movq (%r10), %r12
+
+height5_loop_HW1:
+    movq (%r11), %r12
     cmpq $0, %r12
     je Is_leaf5_HW1
     addq $1, %rax
 
-hight6_HW1:
+height6_HW1:
     addq $1, %rax
     addq $1, %rcx
     addq $8, %r12
 
-sibling5_HW1:
-    addq $8, %r10
-    jmp loop5_HW1
-
-sibling4_HW1:
+height5_next_HW1:
     addq $8, %r11
-    jmp loop4_HW1
+    jmp height5_loop_HW1
 
-sibling3_HW1:
+height4_next_HW1:
+    addq $8, %r10
+    jmp height4_loop_HW1
+
+height3_next_HW1:
     addq $8, %r9
-    jmp loop3_HW1
+    jmp height3_loop_HW1
 
-sibling2_HW1:
+height2_next_HW1:
     addq $8, %r8
-    jmp loop2_HW1
+    jmp height2_loop_HW1
 
-sibling1_HW1:
+height1_next_HW1:
     addq $8, %rdi
-    jmp loop1_HW1
+    jmp height1_loop_HW1
 
 Is_leaf1_HW1:
     cmpq (root), %rdi
-    jne Scan_Done_HW1
+    jne traverse_complete_HW1
     addq $1, %rcx
-    jmp Scan_Done_HW1
+    jmp traverse_complete_HW1
 
 Is_leaf2_HW1:
     cmpq (%rdi), %r8
-    jne sibling1_HW1
+    jne height1_next_HW1
     addq $1, %rcx
-    jmp sibling1_HW1
+    jmp height1_next_HW1
 
 Is_leaf3_HW1:
     cmpq (%r8), %r9
-    jne sibling2_HW1
+    jne height2_next_HW1
     addq $1, %rcx
-    jmp sibling2_HW1 
-
-
+    jmp height2_next_HW1 
 
 Is_leaf4_HW1:
-    cmpq (%r9), %r11
-    jne sibling2_HW1
+    cmpq (%r9), %r10
+    jne height2_next_HW1
     addq $1, %rcx
-    jmp sibling3_HW1
+    jmp height3_next_HW1
 
 Is_leaf5_HW1:
-    cmpq (%r11), %r10
-    jne sibling2_HW1
+    cmpq (%r10), %r11
+    jne height2_next_HW1
     addq $1, %rcx
-    jmp sibling4_HW1
+    jmp height4_next_HW1
 
-Scan_Done_HW1:
+traverse_complete_HW1:
     test %rcx, %rcx      # Check if there are any leaves
-    jz Not_Rich_HW1          # If no leaves, tree is not rich
+    jz Not_Rich_HW1      # If no leaves, tree is not rich
 
     xor %rdx, %rdx       # Clear rdx for division
     div %rcx             # Divide total nodes by total leaves
     cmp $3, %rax         # Compare the quotient with 3
-    jg Not_Rich_HW1          # If quotient > 3, not rich
-    je Equal_Rich_HW1        # If quotient == 3, check remainder
-    jmp Rich_HW1             # Else, rich
+    jg Not_Rich_HW1      # If quotient > 3, not rich
+    je Equal_Rich_HW1    # If quotient == 3, check remainder
+    jmp Rich_HW1         # Else, rich
 
 Not_Rich_HW1:
     movb $0, rich  # Set rich to 0
@@ -122,7 +120,7 @@ Rich_HW1:
 
 Equal_Rich_HW1:
     cmp $0, %rdx         # Check if remainder is 0
-    je Rich_HW1              # If 0, rich
-    jmp Not_Rich_HW1         # Else, not rich
+    je Rich_HW1          # If 0, rich
+    jmp Not_Rich_HW1     # Else, not rich
 
 End_HW1:
